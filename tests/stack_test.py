8373c851142cc524c7bd9bef7cc4e43b4395c7f5
dmitriykodolov <kodolovdv@gmail.com>
2022-06-28 21:48:40 +0300

    ___*___

---
+++ tests/stack_test.py
@@ -0,0 +1,45 @@
+import pytest
+import hexlet_pytest.stack
+
+def test_stack():
+    stack = []
+    # Добавляем два элемента в стек и затем извлекаем их
+    # Почему два? Так надежнее, чем один, а три скорее всего избыточно
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
+
+
+def test_stack1():
+    stack = []
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+
+
+def test_stack2():
+    stack = []
+    stack.append('one')
+    stack.append('two')
+
+    stack.pop()
+    assert stack.pop() == 'one'
+
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)  # not not stack
+
+    stack.pop()
+    assert not stack
+
+
+def test_pop_with_empty_stack():
+    stack = []
+    with pytest.raises(IndexError):
+        stack.pop()
